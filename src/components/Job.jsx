721244042bbc6b9b15e1611e7add2f0642905ec3
Roberto Ramirez <robert.624@hotmail.com>
2023-04-24 12:48:04 -0500

    desktop styles start

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -4,18 +4,26 @@ import './index.scss'
 const Job = ({jobDetails}) => {
   const {company, logo, featured, new: isNew, position, role, level, postedAt, contract, location, languages, tools} = jobDetails
   return (
-    <div className="job-container">
-      <div className="job-header">
-        <img src={logo} alt={company} />
-        <span className="company-name">{company}</span>
-        {isNew && <span className="new">New!</span>}
-        {featured && <span className="featured">Featured</span>}
-      </div>
-      <span className="job-position">{position}</span>
-      <div className="job-misc">
-        <span className="long-ago">{postedAt}</span>
-        <span className="contract">. {contract}</span>
-        <span className="location">. {location}</span>
+    <div className={`job-container ${featured ? 'featured-job': ''}`}>
+      <div className="desktop-left">
+        <div className="desktop-company-image">
+          <img src={logo} alt={company} />
+        </div>
+        <div className="desktop-left-text">
+          <div className="job-header">
+            <img src={logo} alt={company} />
+            <span className="company-name">{company}</span>
+            {isNew && <span className="new">New!</span>}
+            {featured && <span className="featured">Featured</span>}
+          </div>
+          <span className="job-position">{position}</span>
+          <div className="job-misc">
+            <span className="long-ago">{postedAt}</span>
+            <span className="contract">. {contract}</span>
+            <span className="location">. {location}</span>
+          </div>
+        </div>
+
       </div>
       <div className="job-keywords">
         {languages.map((language, i) => (<span key={i} className="keyword">{language}</span>))}
